0fcd819c40da7f774dbb32a954550675589534a3
rstuerm <rileystuermer@gmail.com>
2023-11-22 12:28:16 -0700

    Working Kahoot with score counting

--- kahoot.py
+++ kahoot.py
@@ -14,17 +14,8 @@ import random # Allows randomization of questions pulled for a Kahoot game
 ###############################################################################
 
 GAME_DIMENSION = 700
-BUTTON_DIMENSION = 140
 
-BACKGROUND_COLOR = "white"
-TEXT_COLOR = "black"
-
-DEFAULT_COLOR = 'white'
-CORRECT_COLOR = 'green'
-INCORRECT_COLOR = 'red'
-
-NORMAL_FONT_SIZE = 20
-NORMAL_FONT = ("consolas", NORMAL_FONT_SIZE)
+NORMAL_FONT = ("consolas", 20)
 
 
 ###############################################################################
@@ -33,72 +24,91 @@ NORMAL_FONT = ("consolas", NORMAL_FONT_SIZE)
 
 def start(question_number,score):
 
-    # Remove past buttons
-    for child in window.winfo_children():
-        child.destroy()
+    num_choices = len(choices[question_number])
+    button_dimension = int(GAME_DIMENSION/(num_choices+1))
 
     question_label = tk.Label(text=questions[question_number],
                               wraplength=GAME_DIMENSION,
                               font=NORMAL_FONT,
-                              bg=BACKGROUND_COLOR,
-                              fg=TEXT_COLOR)
+                              bg="white",
+                              fg="black")
 
     question_label.place(width=GAME_DIMENSION,
-                         height=BUTTON_DIMENSION,
+                         height=button_dimension,
                          x=0,
                          y=0)
 
-    button_number = 0
-    for choice in choices[question_number]:
+    for i, choice in enumerate(choices[question_number]):
 
         choice_button = tk.Button(window,
                                   text=choice,
                                   wraplength=GAME_DIMENSION,
                                   font=NORMAL_FONT,
-                                  bg=DEFAULT_COLOR,
-                                  highlightbackground=DEFAULT_COLOR,
-                                  fg=TEXT_COLOR,
-                                  command=lambda selection=choice:
-                                  answer_callback(question_number,choice_button,score)
+                                  bg="white",
+                                  highlightbackground="white",
+                                  fg="black",
+                                  command=lambda choice=choice:
+                                  check_answer(choice,question_number,score)
                                   )
 
         choice_button.place(width=GAME_DIMENSION,
-                            height=BUTTON_DIMENSION,
+                            height=button_dimension,
                             x=0,
-                            y=(button_number+1)*BUTTON_DIMENSION)
+                            y=(i+1)*button_dimension)
 
-        button_number += 1
+def check_answer(choice,question_number,score):
 
-    def answer_callback(question_number,selection,score):
 
-        print(choices[question_number])
-        print(selection)
-        print(answers_list[question_number])
-
-        if selection == answers_list[question_number]:
-            score += 1
-
-        question_number += 1
-
-        # if question_number < total_questions:
-        if question_number < 2:
-            start(question_number,score)
-        else:
-
-            # Remove past buttons
-            for child in window.winfo_children():
-                child.destroy()
-
-            end_label = tk.Label(text='Finished, score: ' + str(score),
-                                 wraplength=GAME_DIMENSION,
-                                 font=NORMAL_FONT,
-                                 bg=BACKGROUND_COLOR,
-                                 fg=TEXT_COLOR)
-
-            end_label.place(width=GAME_DIMENSION,
+    if choice == answers_list[question_number]:
+        print(1)
+        score += 1
+        score_label = tk.Label(text="Correct",
+                                wraplength=GAME_DIMENSION,
+                                font=NORMAL_FONT,
+                                bg="green",
+                                fg="black")
+        
+        score_label.place(width=GAME_DIMENSION,
                             height=GAME_DIMENSION,
                             x=0,
                             y=0)
+    else:
+        print(0)
+        score_label = tk.Label(text="Incorrect",
+                                wraplength=GAME_DIMENSION,
+                                font=NORMAL_FONT,
+                                bg="red",
+                                fg="black")
+        
+        score_label.place(width=GAME_DIMENSION,
+                            height=GAME_DIMENSION,
+                            x=0,
+                            y=0)
+
+    window.after(500, lambda: next_question(question_number, score))
+
+
+def next_question(question_number, score):
+
+    # Remove past buttons
+    for child in canvas.winfo_children():
+        child.destroy()
+
+    question_number += 1
+
+    if question_number < total_questions:
+        start(question_number,score)
+    else:
+        end_label = tk.Label(text='Finished, score: ' + str(score),
+                             wraplength=GAME_DIMENSION,
+                             font=NORMAL_FONT,
+                             bg="white",
+                             fg="black")
+
+        end_label.place(width=GAME_DIMENSION,
+                        height=GAME_DIMENSION,
+                        x=0,
+                        y=0)
 
 ###############################################################################
 #    MAIN:
@@ -115,6 +125,8 @@ for question, answers in webscraping_data.items():
     if answers[1]:
         answers_list.append(answers[1][0])
 
+print(answers_list)
+
 #randomized questions version below ----
 #populate the questions with the questions from the scraped data, and have it so the questions are randomized if we are pulling more than 25 questions 
 # shuffled_list = list(questions_and_choices.items())
@@ -125,6 +137,7 @@ for question, answers in webscraping_data.items():
 #----
 
 total_questions = len(questions)
+total_questions = 10
 
 window = tk.Tk()
 window.title("Kahoot Game")
@@ -132,7 +145,7 @@ window.resizable(False, False)
 
 # Create the canvas for the game.
 canvas = tk.Canvas(window,
-                   bg=BACKGROUND_COLOR,
+                   bg="white",
                    height=GAME_DIMENSION,
                    width=GAME_DIMENSION)
 
