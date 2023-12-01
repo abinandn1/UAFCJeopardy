e3a457aab7286160bc434268a6cf242ddad41ec1
rstuerm <rileystuermer@gmail.com>
2023-11-30 20:09:15 -0700

    Change title of main file, small changes to clean up code

--- main.py
+++ main.py
@@ -2,21 +2,16 @@
 #    INCLUDES:
 ###############################################################################
 
-import time
 import tkinter as tk # Used to create game interface.
-from tkinter import messagebox
 from bs4 import BeautifulSoup
 from webscraping import scrape_questions_and_answers
-import random # Allows randomization of questions pulled for a Kahoot game 
 
 ###############################################################################
 #    CONSTANTS:
 ###############################################################################
 
-GAME_DIMENSION = 700
-
-NORMAL_FONT = ("consolas", 20)
-
+GAME_DIMENSION = 400
+NORMAL_FONT = ("consolas", 10)
 
 ###############################################################################
 #    Functions:
@@ -25,16 +20,16 @@ NORMAL_FONT = ("consolas", 20)
 def start(question_number,score):
 
     num_choices = len(choices[question_number])
-    button_dimension = int(GAME_DIMENSION/(num_choices+1))
+    button_height = int(GAME_DIMENSION/(num_choices+1))
 
     question_label = tk.Label(text=questions[question_number],
-                              wraplength=GAME_DIMENSION,
                               font=NORMAL_FONT,
                               bg="white",
-                              fg="black")
+                              fg="black",
+                              wraplength=GAME_DIMENSION)
 
     question_label.place(width=GAME_DIMENSION,
-                         height=button_dimension,
+                         height=button_height,
                          x=0,
                          y=0)
 
@@ -52,15 +47,14 @@ def start(question_number,score):
                                   )
 
         choice_button.place(width=GAME_DIMENSION,
-                            height=button_dimension,
+                            height=button_height,
                             x=0,
-                            y=(i+1)*button_dimension)
+                            y=(i+1)*button_height)
 
 def check_answer(choice,question_number,score):
 
 
     if choice == answers_list[question_number]:
-        print(1)
         score += 1
         score_label = tk.Label(text="Correct",
                                 wraplength=GAME_DIMENSION,
@@ -73,7 +67,6 @@ def check_answer(choice,question_number,score):
                             x=0,
                             y=0)
     else:
-        print(0)
         score_label = tk.Label(text="Incorrect",
                                 wraplength=GAME_DIMENSION,
                                 font=NORMAL_FONT,
@@ -125,36 +118,19 @@ for question, answers in webscraping_data.items():
     if answers[1]:
         answers_list.append(answers[1][0])
 
-print(answers_list)
-
-#randomized questions version below ----
-#populate the questions with the questions from the scraped data, and have it so the questions are randomized if we are pulling more than 25 questions 
-# shuffled_list = list(questions_and_choices.items())
-# random.shuffle(shuffled_list)
-# shuffled_data = dict(shuffled_list)
-# questions = list(shuffled_data.keys())
-# choices = list(shuffled_data.values())
-#----
-
 total_questions = len(questions)
-total_questions = 10
 
 window = tk.Tk()
 window.title("Kahoot Game")
 window.resizable(False, False)
 
 # Create the canvas for the game.
-canvas = tk.Canvas(window,
-                   bg="white",
-                   height=GAME_DIMENSION,
-                   width=GAME_DIMENSION)
+canvas = tk.Canvas(window, bg="white", height=GAME_DIMENSION, width=GAME_DIMENSION)
 
 # Pack the canvas.
 canvas.pack()
 
-# Update the game window.
-window.update()
-
+# Start the game
 start(0,0)
 
 # Run the main loop of the program.
